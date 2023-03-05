cd7b577de7a3c9fc55a4c5ad2f2ab3ee387f9542
hjs9605 <hjs9605@naver.com>
2023-03-05 18:07:08 +0900

    firstcommit

---
+++ src/test/java/com/example/befirst2/Befirst2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.befirst2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Befirst2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
